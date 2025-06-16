cbf561e6486c82c1ef195db1e0742e7eadac52b4
gabriele <monti.g@gmail.com>
2025-06-16 15:13:03 +0200

    added port binding

--- app.py
+++ app.py
@@ -217,3 +217,10 @@ def root():
 
 
 mount_gradio_app(app, demo, path="/gradio")
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.getenv("PORT", 10000))  # use PORT env var or default to 10000
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
